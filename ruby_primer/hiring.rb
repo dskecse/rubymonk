ad855b1ecf456511e55e8c1adbbf5db504c74ab6
Dzianis Dashkevich <dskecse@gmail.com>
2015-01-18 17:54:18 +0300

    Add solution to hiring programmers problem

---
+++ ruby_primer/hiring.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+is_an_experienced_programmer =
+  candidate.languages_worked_with.include?('Ruby') &&
+  (candidate.years_of_experience >= 2 || candidate.github_points >= 500) &&
+  !(candidate.age < 15 || candidate.applied_recently?)
